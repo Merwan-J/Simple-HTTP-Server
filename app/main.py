f4a5bed2f50b23588fa86021b14c58aee8ad8dee
Merwan-J <merwanj71@gmail.com>
2024-07-13 15:14:13 +0300

    extract url path

--- app/main.py
+++ app/main.py
@@ -2,14 +2,26 @@ import socket
 
 
 def main():
+    def extract_request_line(request):
+        request_line = request.split("\r\n")[0]
+        method, path, _ = request_line.split(" ")
+        return method, path
+    
     with socket.create_server(("localhost", 4221)) as socket_server:
-        while True: # keep the server running
+        print("Server started at port 4221")
+        while True: 
             conn, addr = socket_server.accept()
             with conn:
-                data = "HTTP/1.1 200 OK\r\n\r\n".encode()
-                conn.sendall(data)
-                print("Sent data to client with address: ", addr)
-                print("Data sent: ", data.decode())
+                request = conn.recv(1024)
+                data = request.decode()
+                method, path = extract_request_line(data)
+
+                if path == "/":
+                    msg = f"HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    msg = f"HTTP/1.1 404 Not Found\r\n\r\n"
+                
+                conn.sendall(msg.encode())
 
 
                 
